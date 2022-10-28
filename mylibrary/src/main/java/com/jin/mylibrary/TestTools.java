38803d969b080337a13bb7c9a12190bc62ac719e
jinhuiyu0403 <jinhuiyu0403@126.com>
2022-10-28 09:34:03 +0800

    eee

--- mylibrary/src/main/java/com/jin/mylibrary/TestTools.java
+++ mylibrary/src/main/java/com/jin/mylibrary/TestTools.java
@@ -3,6 +3,6 @@ package com.jin.mylibrary;
 public class TestTools {
     public float result;
     public TestTools(float a, float b){
-        result = a + b;
+        result = a + b * b;
     }
 }
