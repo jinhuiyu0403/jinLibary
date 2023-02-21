77de93ef182aefd41092a5ee58cc79317f0772dd
jinhuiyu0403 <jinhuiyu0403@126.com>
2023-02-21 14:43:08 +0800

    增加数据转换Float To Byte

--- mylibrary/src/main/java/com/jin/mylibrary/DisplayUtil.java
+++ mylibrary/src/main/java/com/jin/mylibrary/DisplayUtil.java
@@ -1,5 +1,6 @@
 package com.jin.mylibrary;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
@@ -13,6 +14,27 @@ public class DisplayUtil {
     public static void SetLandscapeDisplay(Activity activity){
         //屏幕横屏
         activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
+            lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            activity.getWindow().setAttributes(lp);
+        }else{
+            activity.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+            activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+//        //屏幕不休眠且全屏
+        activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_FULLSCREEN
+                , WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        final View decorView = activity.getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    //设置屏幕竖屏且永不熄屏
+    @SuppressLint("SourceLockedOrientationActivity")
+    public static void SetPortraitDisplay(Activity activity){
+        //屏幕横屏
+        activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
             lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
--- mylibrary/src/main/java/com/jin/mylibrary/TestTools.java
+++ mylibrary/src/main/java/com/jin/mylibrary/TestTools.java
@@ -1,5 +1,17 @@
 package com.jin.mylibrary;
 
+import android.util.Log;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestTools {
 
     //https://github.com/jinhuiyu0403/jinLibary 上release
@@ -8,5 +20,46 @@ public class TestTools {
     public float result;
     public TestTools(float a, float b){
         result = a + b * b;
+    }
+    //gson与实体类互转
+//    private void SaveData(){
+//        ArrayList<CreatePara> list = new ArrayList<>();
+//        builder = new GsonBuilder();
+//        gson = builder.create();
+//        list.add(new CreatePara(1,2,3,4));
+//        list.add(new CreatePara(2,4,5,6));
+//        String jsontext = gson.toJson(list);
+//        List<CreatePara> newlist = new ArrayList<>();
+//        @SuppressWarnings("rawtypes") Type typ1 = new TypeToken<List<CreatePara>>(){}.getType();
+//        newlist = gson.fromJson(jsontext,typ1);
+//        Log.d("Test",jsontext);
+//    }
+
+    private String testStr1 = "{\"list\":[{\"name\":\"检验日期\",\"value\":\"10.30\"},{\"name\":\"使用单位名称\",\"value\":\"大连\"},{\"name\":\"设备代码\",\"value\":\"210012\"},{\"name\":\"设备类别\",\"value\":\"厂车\"},{\"name\":\"设备品种\",\"value\":\"手刹\"},{\"name\":\"检验机构名称\",\"value\":\"恒亚\"},{\"name\":\"检验人员\",\"value\":\"顾\"},{\"name\":\"最大手操纵力\",\"value\":\"4.4N\"},{\"name\":\"文件链接\",\"value\":\"http:\\/\\/cdn.ln-hy.com\\/dlhy_20221103181121_c92c8c7ddf888bdd2bba424a6a66eb25.doc\"}]}";
+    private String testStr2 = "[{\"name\":\"检验日期\",\"value\":\"10.30\"},{\"name\":\"使用单位名称\",\"value\":\"大连\"},{\"name\":\"设备代码\",\"value\":\"210012\"},{\"name\":\"设备类别\",\"value\":\"厂车\"},{\"name\":\"设备品种\",\"value\":\"手刹\"},{\"name\":\"检验机构名称\",\"value\":\"恒亚\"},{\"name\":\"检验人员\",\"value\":\"顾\"},{\"name\":\"最大手操纵力\",\"value\":\"4.4N\"},{\"name\":\"文件链接\",\"value\":\"http:\\/\\/cdn.ln-hy.com\\/dlhy_20221103181121_c92c8c7ddf888bdd2bba424a6a66eb25.doc\"}]";
+    private String[] GetRecvJson(String testStr){
+        String[] recv;
+        try{
+            if(testStr.contains("list")){
+                JSONObject jsonObject = new JSONObject(testStr);
+                JSONArray jsonArray = jsonObject.getJSONArray("list");
+                recv = new String[jsonArray.length()];
+                for(int i = 0;i < jsonArray.length();i++){
+                    JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                    recv[i] = jsonObject1.getString("value");
+                }
+            }else{
+                JSONArray jsonArray = new JSONArray(testStr);
+                recv = new String[jsonArray.length()];
+                for(int i = 0;i < jsonArray.length();i++){
+                    JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                    recv[i] = jsonObject1.getString("value");
+                }
+            }
+            return recv;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
